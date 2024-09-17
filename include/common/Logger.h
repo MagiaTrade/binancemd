04455cc8f82f6d89de8d0bc87a6d933eafba3f58
Tato Levicz <leviczios@gmail.com>
2024-09-17 16:41:11 -0300

    fixed lib name bb + tests building

--- include/common/Logger.h
+++ include/common/Logger.h
@@ -5,4 +5,28 @@
 #ifndef BINANCEMD_LOGGER_H
 #define BINANCEMD_LOGGER_H
 
+#include <memory>
+#include <mgutils/Logger.h>
+
+namespace bmd
+{
+  class Logger
+  {
+  public:
+    inline static mgutils::Logger& instance()
+    {
+      static mgutils::Logger logger;
+      return logger;
+    }
+  };
+}
+
+#define logT bmd::Logger::instance().log(mgutils::Trace)
+#define logD bmd::Logger::instance().log(mgutils::Debug)
+#define logI bmd::Logger::instance().log(mgutils::Info)
+#define logW bmd::Logger::instance().log(mgutils::Warning)
+#define logE bmd::Logger::instance().log(mgutils::Error)
+#define logC bmd::Logger::instance().log(mgutils::Critical)
+
+
 #endif //BINANCEMD_LOGGER_H
--- tests/cases/streams_tests.cpp
+++ tests/cases/streams_tests.cpp
@@ -1,3 +1,55 @@
 //
 // Created by Arthur Motelevicz on 17/09/24.
 //
+#include <binancemd>
+#include <catch2/catch.hpp>
+#define SYMBOL_TO_TEST "wdov24"
+
+TEST_CASE("COMMANDS")
+{
+//cedro::md::CMDBaseManager manager(CEDRO_USERNAME, CEDRO_PASSWORD, CEDRO_SOFTKEY);
+//
+//std::promise<bool> sendPromise;
+//std::future<bool> sendFuture = sendPromise.get_future();
+//
+//std::shared_ptr<bb::network::rs::Stream> streamPtr;
+//
+//manager.connect([&](bool success, const std::shared_ptr<bb::network::rs::Stream>& stream)
+//{
+//streamPtr = stream;
+//sendPromise.set_value(success);
+//});
+//
+//TestHelper::waitForSuccess(sendFuture, streamPtr, 3);
+//
+//manager.setErrorCallback([&](const cedro::md::Error& error, const std::string& msg)
+//{
+//LOG_WARNING("Error: " + error.toString() + " Msg: " + msg);
+//});
+//
+//SECTION("SQT snapshot")
+//{
+//std::promise<bool> sendSQTPromise;
+//std::future<bool> sendSQTFuture = sendSQTPromise.get_future();
+//bool isSnapshot = true;
+//bool promiseSet = false;
+//
+//manager.subscribeQuote(SYMBOL_TO_TEST,
+//[&](bool success, const char* data, size_t size)
+//{
+//if(promiseSet)
+//return;
+//
+//auto *startMsg = std::strstr(data, "T:");
+//auto *endMsg = std::strchr(data, '!');
+//REQUIRE(startMsg != nullptr);
+//REQUIRE(endMsg != nullptr);
+//REQUIRE(success);
+//REQUIRE(size > 250);
+//LOG_INFO(data);
+//sendSQTPromise.set_value(success);
+//promiseSet = true;
+//
+//}, isSnapshot);
+
+}
